7d25e6d4c48fe45f88d7b86bba6ef24fc1dd89b2
TanushDhiman <dhimantanush687@gmail.com>
2026-05-27 21:55:28 +0530

    first commit

--- ChatBot/src/constans.js
+++ ChatBot/src/constans.js
@@ -1,1 +1,1 @@
-export const URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyBjCeGlbVfxd28ZnlWJjMVGB2ntrSoWfUA'
+export const URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyAAYEMVGKjACgCaBrl3MgsyAAtPlQaywpo'
