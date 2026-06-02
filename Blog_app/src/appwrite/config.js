2433b43d21e808a94bb4a664f10d9eac1fabf140
TanushDhiman <dhimantanush687@gmail.com>
2026-06-02 23:50:26 +0530

    first commit

--- Blog_app/src/appwrite/config.js
+++ Blog_app/src/appwrite/config.js
@@ -1,18 +1,18 @@
 import conf from '../conf.js';
-import { Client, Account, ID , Databases ,Storage ,Query} from "appwrite";
+import { Client, Account, ID, Databases, Storage, Query } from "appwrite";
 
 
 export class Dataservice {
     Client = new Client
     databases;
     bucket;
-    constructor(){
+    constructor() {
         this.Client
             .setProject(conf.appwriteProjectId)
             .setEndpoint(conf.appwriteUrl);
     }
-    
-    async createPost ({tittle,slug,content,featuredImage,status,userId}){
+
+    async createPost({ tittle, slug, content, featuredImage, status, userId }) {
         try {
             return await this.databases.createdocument(
                 conf.appwriteDatabaseId,
@@ -28,10 +28,10 @@ export class Dataservice {
             )
         } catch (error) {
             console.log("appwrite server :: createPost::error", error);
-            
+
         }
     }
-    async updatePost (slug,{tittle,content,featuredImage,status,}){
+    async updatePost(slug, { tittle, content, featuredImage, status, }) {
         try {
             return await this.databases.updateDocument(
                 conf.appwriteDatabaseId,
@@ -42,17 +42,17 @@ export class Dataservice {
                     content,
                     featuredImage,
                     status,
-                    
+
                 }
             )
         } catch (error) {
-            console.log("appwrite server :: updatePost::error", error);
-            
+            console.log("appwrite server ::updatePost::error", error);
+
         }
     }
-    async deletePost(slug){
+    async deletePost(slug) {
         try {
-             await this.databases.deleteDocument(
+            await this.databases.deleteDocument(
                 conf.appwriteDatabaseId,
                 conf.appwriteCollectionId,
                 slug,
@@ -61,6 +61,66 @@ export class Dataservice {
         } catch (error) {
             console.log("appwrite server :: deletePost::error", error);
             return false
+        }
+    }
+    async getPost(slug) {
+        try {
+            await this.databases.getDocument(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                slug,
+
+            )
+        } catch (error) {
+            console.log("appwrite server :: getPost::error", error);
+            return false;
+        }
+    }
+    async getPosts(queries = [Query.equal("status", "active")]) {
+        try {
+            return await this.databases.listDocuments(
+                conf.appwriteDatabaseId,
+                conf.appwriteCollectionId,
+                queries,
+            )
+        } catch (error) {
+            console.log("appwrite server :: getPosts::error", error);
+            return false;
+        }
+    }
+    async uploadFile(file) {
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file,
+            )
+        } catch (error) {
+            console.log("appwrite server :: uploadFile::error", error);
+            return false;
+        }
+    }
+    async deleteFile(fileId) {
+        try {
+             await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId,
+            )
+            return true;
+        } catch (error) {
+            console.log("appwrite server :: deleteFile::error", error);
+            return false;
+        }
+    }
+    async getFilePreview(fileId){
+        try {
+            return await this.bucket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId,
+            )
+        } catch (error) {
+            console.log("appwrite server :: getFilePreview::error", error);
+            return false;
         }
     }
 }
