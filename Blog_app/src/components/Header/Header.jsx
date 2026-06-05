9e178d921abad35a6000097e4c159d7ea13e388d
TanushDhiman <dhimantanush687@gmail.com>
2026-06-05 14:18:57 +0530

    first commit

--- Blog_app/src/components/Header/Header.jsx
+++ Blog_app/src/components/Header/Header.jsx
@@ -44,6 +44,23 @@ function Header() {
                     <Logo width='80px'/>
                     </Link>
                   </div>
+                  <ul className='flex ml-auto'>
+                    {navItems.map((item)=>
+                        item.active ? (
+                            <li key={item.name}>
+                                <button 
+                                onClick={()=>navigate(item.slug)}
+                                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                                >{item.name}</button>
+                            </li>
+                        ):null
+                        )}
+                        {authStatus && (
+                            <li>
+                                <LogoutBtn/>
+                            </li>
+                        )}
+                  </ul>
                 </nav>
             </Container>
         </header>
